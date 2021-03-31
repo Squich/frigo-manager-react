caa5b522fa4102ab051d2fdede6906355b0d00d1
Squich <quichaud.stephane@gmail.com>
2021-03-31 22:39:35 +0200

    Fix bg-landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -12,12 +12,12 @@ const Landing = props => {
     return (
         <div className="bg-landing h-100 d-flex flex-column justify-content-center align-items-center">
             <Link to="/login">
-                <button type="button" className="btn btn-primary btn-lg m-3">
+                <button type="button" className="btn btn-primary btn-lg m-2">
                     Connexion
                 </button>
             </Link>
             <Link to="/signup">
-                <button type="button" className="btn btn-secondary btn-lg m-3">
+                <button type="button" className="btn btn-secondary btn-lg m-2">
                     Inscription
                 </button>
             </Link>
